6fd420a1762aa36e04ef05ea5caf6b17776b5694
dinaamlll <dinamaliaa17@gmail.com>
2023-06-29 18:49:46 +0700

    void preorder

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -85,5 +85,20 @@ void inorder(Node* ptr)
 		inorder(ptr->rightchild);
 	}
 }
+
+void preorder(Node* ptr)
+{
+	if (ROOT == NULL)
+	{
+		cout << "Tree is empty" << endl;
+		return;
+	}
+	if (ptr != NULL)
+	{
+		cout << ptr->info << " ";
+		preorder(ptr->leftchild);
+		preorder(ptr->rightchild);
+	}
+}
 		
 	
