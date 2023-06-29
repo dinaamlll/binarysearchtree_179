c0c7a6dc9f91be3bede5cc50581f46dd5dccc31a
dinaamlll <dinamaliaa17@gmail.com>
2023-06-29 18:06:32 +0700

    if (element < parent->info)

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -43,3 +43,13 @@ void insert(string element) // Insert a node in the binary search tree
 		ROOT = newNode; // Mark the new node as ROOT
 		return; // Exit
 	}
+
+	if (element < parent->info) // if the value in the data field of the new node is less than that of the parent
+	{
+		parent->leftchild = newNode; // Make the left child of the parent point to the new node
+	}
+	else if (element > parent->info) // If the value data field of the new node is greater than that of the parent
+	{
+		parent->rightchild = newNode; // Make the right child of the parent point to the new node
+	}
+}
